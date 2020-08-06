08d8913615d9d2c5f1ebabce7ecbe06ab2cba363
Faber Vergara <fabervergara@gmail.com>
2020-08-05 23:44:54 -0500

    add our team section

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 $areasArray = new WP_Query(array(
   'post_type' => 'areas-de-practica',
   'post_status' => 'publish'
+));
+
+$ourTeamArray = new WP_Query(array(
+  'post_type' => 'nuestro-equipo',
+  'post_status' => 'publish'
 )); ?>
 
   <div class="container mx-auto">
@@ -32,18 +37,20 @@ $areasArray = new WP_Query(array(
         <img src="<?php echo get_template_directory_uri() ?>/images/lustitia.png" alt="" class="img-fluid">
       </div>
       <div class="col-md-6">
-        <h2 class="title font-blue">Compromiso y experiencia</h2>
-        <h3 class="subtitle font-golden">al servicio de tu caso</h3>
+        <h2 class="main-title font-blue">Compromiso y experiencia</h2>
+        <h3 class="main-subtitle font-golden">al servicio de tu caso</h3>
         <p class="mt-3">Si necesitas asesoría en tus conflictos, nuestro equipo jurídico está aquí para ayudarte.</p>
 
         <a href="#" class="btn btn-dark btn-action">Solicita asesoría gratuita</a>
       </div>
     </div>
+
     <div class="row mt-5">
       <div class="col">
         <p class="font-golden">Áreas de práctica</p>
       </div>
     </div>
+
     <div class="row">
       <div class="col">
         <ul class="list-inline" id="areas-de-practica">
@@ -53,6 +60,35 @@ $areasArray = new WP_Query(array(
         </ul>
       </div>
     </div>
-  </div>
 
+    <div class="row mt-5">
+      <div class="col">
+        <h3 class="subtitle">Nuestro equipo</h3>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-md-6">
+        <p>C&C Abogados ofrece asesoramiento legal y defensa judicial. Analizamos el caso y buscamos la solución más adecuada para nuestros clientes.</p>
+      </div>
+    </div>
+    <div class="row-cols-1 mt-3" id="our-team">
+    <?php while($ourTeamArray->have_posts()): $ourTeamArray->the_post() ?>
+      <div class="col mb-3">
+        <div class="card border-0">
+          <div class="row no-gutters">
+            <div class="col-md-3 rounded-lg">
+              <?php the_post_thumbnail('full', ['class' => 'card-img']) ?>
+            </div>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title font-golden"><?php echo the_title() ?></h5>
+                <p class="card-text"><?php the_content() ?></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php endwhile ?>
+    </div>
+  </div>
 <?php get_footer() ?>
