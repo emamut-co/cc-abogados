0b029411eb3ade34cc84b4607ef95b98977e7c35
Faber Vergara <fabervergara@gmail.com>
2020-08-05 23:06:27 -0500

    fix layout

--- index.php
+++ index.php
@@ -1,6 +1,39 @@
 <?php get_header(); ?>
 
   <div class="container mx-auto">
+    <div class="row justify-content-end mt-4">
+      <div class="col text-right">
+        <ul class="list-inline" id="social-icons">
+          <li class="list-inline-item px-3">
+            <a href="#" class="mr-3">
+              <i class="fab fa-facebook-f fa-lg"></i>
+            </a>
+          </li>
+          <li class="list-inline-item px-3">
+            <a href="#" class="mr-3">
+              <i class="fab fa-instagram fa-lg"></i>
+            </a>
+          </li>
+          <li class="list-inline-item px-3">
+            <a href="#" class="mr-3">
+              <i class="fab fa-linkedin-in fa-lg"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-2">
+        <img src="<?php echo get_template_directory_uri() ?>/images/lustitia.png" alt="" class="img-fluid">
+      </div>
+      <div class="col-md-6">
+        <h2 class="title">Compromiso y experiencia</h2>
+        <h3 class="subtitle">al servicio de tu caso</h3>
+        <p class="mt-3">Si necesitas asesoría en tus conflictos, nuestro equipo jurídico está aquí para ayudarte.</p>
+
+        <a href="#" class="btn btn-dark btn-action">Solicita asesoría gratuita</a>
+      </div>
+    </div>
   </div>
 
 <?php get_footer() ?>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,18 @@
 let mix = require('laravel-mix');
 
-mix.autoload({
-    jquery: ["$", "window.jQuery", "jQuery"],
-    "popper.js/dist/umd/popper.js": ["Popper"],
+mix
+  .autoload({
+    jquery: ['$', 'window.jQuery', 'jQuery'],
+    'popper.js/dist/umd/popper.js': ['Popper'],
   })
-  .js("src/js/app.js", "dist/js")
-  .sass("src/sass/app.sass", "dist/css")
+  .options({
+    processCssUrls: false,
+  })
+  .js('src/js/app.js', 'dist/js')
+  .sass('src/sass/app.sass', 'dist/css')
   .browserSync({
-    proxy: "http://localhost/cc-abogados/",
+    proxy: 'http://localhost/cc-abogados/',
     open: false,
-    files: ["dist/css/app.css", "dist/js/app.js", "./**/*.+(html|php)"],
+    files: ['dist/css/app.css', 'dist/js/app.js', './**/*.+(html|php)'],
   })
-  .sourceMaps(true, "source-map");
+  .sourceMaps(true, 'source-map');
