f18f17055e31c3e2c725f7b46f414001d17a254e
Faber Vergara <fabervergara@gmail.com>
2020-08-05 23:21:43 -0500

    add .font-golden and .font-blue

--- index.php
+++ index.php
@@ -27,11 +27,16 @@
         <img src="<?php echo get_template_directory_uri() ?>/images/lustitia.png" alt="" class="img-fluid">
       </div>
       <div class="col-md-6">
-        <h2 class="title">Compromiso y experiencia</h2>
-        <h3 class="subtitle">al servicio de tu caso</h3>
+        <h2 class="title font-blue">Compromiso y experiencia</h2>
+        <h3 class="subtitle font-golden">al servicio de tu caso</h3>
         <p class="mt-3">Si necesitas asesoría en tus conflictos, nuestro equipo jurídico está aquí para ayudarte.</p>
 
         <a href="#" class="btn btn-dark btn-action">Solicita asesoría gratuita</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+
       </div>
     </div>
   </div>
