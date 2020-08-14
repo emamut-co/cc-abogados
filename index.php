fb41713ce3b22ac19535c1b1bfac8f928adb63af
Faber Vergara <fabervergara@gmail.com>
2020-08-14 05:11:36 -0500

    add golden border bottom

--- index.php
+++ index.php
@@ -63,8 +63,8 @@ $casesArray = new WP_Query(array(
     </div>
 
     <div class="row">
-      <div class="col">
-        <ul class="list-inline" id="areas-de-practica">
+      <div class="col" id="areas-de-practica">
+        <ul class="list-inline">
         <?php while($areasArray->have_posts()): $areasArray->the_post() ?>
           <li class="list-inline-item"><?php echo the_title() ?></li>
         <?php endwhile ?>
