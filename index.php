09ec43a0808c29ccadff8c7678955a93ce9f5b1a
Faber Vergara <fabervergara@gmail.com>
2020-08-21 23:28:12 -0500

    fix layout

--- index.php
+++ index.php
@@ -154,7 +154,7 @@ $casesArray = new WP_Query(array(
               <div class="col mb-4">
                 <div class="card rounded-0">
                   <div class="card-body">
-                    <h5 class="card-title font-times text-golden"><?php the_title() ?></h5>
+                    <a href="<?php echo the_permalink() ?>" class="card-title font-times text-golden"><h5><?php the_title() ?></h5></a>
                     <p class="card-text"><?php the_excerpt() ?></p>
                     <a href="<?php echo the_permalink() ?>" class="card-text mt-3 text-golden">
                       <img src="<?php echo get_template_directory_uri() ?>/images/arrow-right.png" alt="" class="img-fluid"> Leer más
